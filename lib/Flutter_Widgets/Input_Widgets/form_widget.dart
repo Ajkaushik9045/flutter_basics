a6a89e4f5edc49b5652290d96d619517097e0d30
Ajkaushik9045 <staraj89@gmail.com>
2024-08-20 23:08:45 +0530

    FormWidget

---
+++ lib/Flutter_Widgets/Input_Widgets/form_widget.dart
@@ -0,0 +1,125 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class FormWidget extends StatefulWidget {
+  const FormWidget({super.key});
+
+  @override
+  State<FormWidget> createState() => _FormWidgetState();
+}
+
+class _FormWidgetState extends State<FormWidget> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  void submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Validation Successfull"),
+        ),
+      );
+    }
+  }
+
+  String? emailValidator(value) {
+    if (value!.isEmpty) {
+      return 'Please enter your email';
+    }
+    return null;
+  }
+
+  String? phoneValidator(value) {
+    if (value!.isEmpty) {
+      return "Enter Phone number";
+    }
+    if (value.length != 10) {
+      return "Enter valid Phone number";
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Form Widget"),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: InputDecoration(
+                      labelText: "Username",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please Enter your name";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                    ),
+                    validator: emailValidator,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: InputDecoration(
+                      labelText: "Phone Number",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20),
+                        ),
+                      ),
+                    ),
+                    validator: phoneValidator,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: submitForm,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 40,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      child: Text(
+                        "Submit",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                  )
+                ],
+              )),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ import 'package:flutter_basics/Flutter_Widgets/Basic_Widgets/text_widget.dart';
 import 'package:flutter_basics/Flutter_Widgets/Input_Widgets/checkBox_widget.dart';
 import 'package:flutter_basics/Flutter_Widgets/Input_Widgets/check_box_list_tile.dart';
 import 'package:flutter_basics/Flutter_Widgets/Input_Widgets/drop_down_button.dart';
+import 'package:flutter_basics/Flutter_Widgets/Input_Widgets/form_widget.dart';
 import 'package:flutter_basics/Flutter_Widgets/Input_Widgets/radio_list_tiles.dart';
 import 'package:flutter_basics/Flutter_Widgets/Input_Widgets/radio_widget.dart';
 import 'package:flutter_basics/Flutter_Widgets/Input_Widgets/slider_widget.dart';
@@ -51,7 +52,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DropDownButtonWidget(),
+      home: FormWidget(),
     );
   }
 }
